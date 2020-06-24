764fa57b24368d965a0a743565eff2874804d2e9
Rithik Jain <rithik.jain3006@gmail.com>
2020-06-24 19:36:53 +0530

    Base app with dummy UI setup

--- app/src/main/java/com/rithikjain/nstvideo/MainActivity.kt
+++ app/src/main/java/com/rithikjain/nstvideo/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.rithikjain.nstvideo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var mJob: Job
+    override val coroutineContext: CoroutineContext
+        get() = mJob + Dispatchers.Main
+
+    private var isModelRunning = false
+    private var contentImage: Bitmap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        progressBar.visibility = View.INVISIBLE
+        mJob = Job()
+
+        val styleList = mutableListOf<String>()
+        val styleNameList = mutableListOf<String>()
+        styleList.add("none.png")
+        styleNameList.add("None")
+        for (i in 1..26) {
+            styleList.add("style$i.jpg")
+            styleNameList.add("Style $i")
+        }
+
+        val borderDrawable = ContextCompat.getDrawable(this, R.drawable.image_border)
+        val stylesRecyclerViewAdapter = StylesRecyclerViewAdapter(borderDrawable!!)
+        stylesRecyclerViewAdapter.setStylesList(styleList, styleNameList)
+        stylesRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            adapter = stylesRecyclerViewAdapter
+        }
+        LinearSnapHelper().attachToRecyclerView(stylesRecyclerView)
+
+        stylesRecyclerView.addOnItemClickListener(object: OnItemClickListener {
+            override fun onItemClicked(position: Int, view: View) {
+                if (!isModelRunning) {
+                    if (contentImage != null) {
+                        isModelRunning = true
+                        stylesRecyclerViewAdapter.notifyItemChanged(stylesRecyclerViewAdapter.selectedPos)
+                        stylesRecyclerViewAdapter.selectedPos = position
+                        stylesRecyclerViewAdapter.notifyItemChanged(stylesRecyclerViewAdapter.selectedPos)
+                        if (position == 0) {
+                            outputImage.setImageBitmap(contentImage)
+                            isModelRunning = false
+                        } else {
+
+                        }
+                    } else {
+                        Toast.makeText(
+                            this@MainActivity,
+                            "Select an image first",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                } else {
+                    Log.d("esh", "Model Running")
+                }
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/rithikjain/nstvideo/StylesRecyclerViewAdapter.kt
@@ -0,0 +1,59 @@
+package com.rithikjain.nstvideo
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.style_item.view.*
+
+class StylesRecyclerViewAdapter(private val border: Drawable) :
+    RecyclerView.Adapter<StylesRecyclerViewAdapter.StylesViewHolder>() {
+
+    private var stylesList: List<String> = listOf()
+    private var stylesNameList: List<String> = listOf()
+    var selectedPos = 0
+
+    fun setStylesList(styles: List<String>, styleNames: List<String>) {
+        stylesList = styles
+        stylesNameList = styleNames
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = StylesViewHolder(
+        LayoutInflater.from(parent.context).inflate(
+            R.layout.style_item,
+            parent,
+            false
+        ),
+        parent.context
+    )
+
+    override fun onBindViewHolder(holder: StylesViewHolder, position: Int) {
+        holder.bind(stylesList[position], stylesNameList[position])
+
+        if (selectedPos == position) {
+            holder.itemView.image.foreground = border
+        } else {
+            holder.itemView.image.foreground = null
+        }
+    }
+
+    override fun getItemCount(): Int = stylesList.size
+
+    class StylesViewHolder(view: View, private val context: Context) :
+        RecyclerView.ViewHolder(view) {
+        private val imageView = view.image
+        private val title = view.title
+
+        fun bind(imageName: String, styleName: String) {
+            Glide.with(context)
+                .load(Uri.parse("file:///android_asset/styles/$imageName"))
+                .into(imageView)
+            title.text = styleName
+        }
+    }
+}
